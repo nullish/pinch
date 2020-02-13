217466194a67de0add0055bf0dd8b54361fad54f
Richard Eason <r.eason@shu.ac.uk>
2020-02-13 16:52:42 +0000

    Detect XML or JSON and load file

--- src/pinch.js
+++ src/pinch.js
@@ -5,5 +5,29 @@
 */
 
 const puppeteer = require('puppeteer')
-fs = require('fs');
-var parser = require('xml2json');
+fs = require('fs')
+let parser = require('xml2json')
+
+// MAIN MODULE TO EXPORT
+const pinch = async() => {
+	// Load config file
+	const cf = require("../pinch.config.json")
+	// If XML convert to JSON
+	let urlSet
+	if (cf.urlSourcePath.match(/\.xml$/g)) {
+		urlSet = convertJson(cf.urlSourcePath)
+	} else {
+		urlSet = JSON.parse(fs.readFileSync(cf.urlSourcePath))
+	}
+	console.log(urlSet)
+}
+
+function convertJson(inPath) {
+	// Converts an XML file to JSON object
+	const xmlFile = fs.readFileSync(inPath)
+	const jsonEqv = parser.toJson(xmlFile)
+  	const json = JSON.parse(jsonEqv)
+  	return json
+}
+
+pinch()
